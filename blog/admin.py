f8852373fc9fe7e3d590ddce36ba80e53c1bf904
felipetf7 <felipenews2016@gmail.com>
2024-08-30 21:20:27 -0300

    django and models

---
+++ blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Gostos, Cursos, Comidas
+# Register your models here.
+admin.site.register(Gostos)
+
+admin.site.register(Cursos)
+
+admin.site.register(Comidas)
---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Gostos(models.Model):
+    nome = models.CharField(max_length=50)
+    descricao = models.TextField()
+    ranking = models.CharField(max_length=1)
+
+class Cursos(models.Model):
+    nome = models.CharField(max_length=50)
+    descricao = models.TextField()
+    ranking_utilidade = models.CharField(max_length=1)
+
+class Comidas(models.Model):
+    nome = models.CharField(max_length=50)
+    descricao = models.TextField()
+    ranking = models.CharField(max_length=1)
